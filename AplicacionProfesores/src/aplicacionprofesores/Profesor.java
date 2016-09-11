cd3bdada5bb1a8b4a68bd04fd6aaa36222458725
Javier117 <>
2016-09-11 15:22:00 -0500

    interfaz y clase profesor

---
+++ AplicacionProfesores/src/aplicacionprofesores/Profesor.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacionprofesores;
+
+/**
+ *
+ * @author usuario
+ */
+public class Profesor {
+    String nombre;
+    String id;
+    String correo;
+    int tel;
+    int edad;
+    String jornada;
+    String facultad;
+    String programa;
+    int fallas;
+    int llegadastarde;
+    
+    public Profesor(String name,String ident,int age,String mail,int cel,String Jornada,String Fac,String Programa)
+    {
+        this.nombre=name;
+        this.id=ident;
+        this.edad=age;
+        this.correo=mail;
+        this.tel=cel;
+        this.jornada=Jornada;
+        this.facultad=Fac;
+        this.programa=Programa;
+        this.llegadastarde=0;
+        this.fallas=0;
+    
+    }
+
+    
+    
+    
+    
+    
+    // metodos por defecto
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(String jornada) {
+        this.jornada = jornada;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getPrograma() {
+        return programa;
+    }
+
+    public void setPrograma(String programa) {
+        this.programa = programa;
+    }
+    
+
+    
+    
+}
